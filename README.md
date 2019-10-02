c742b14e60b50fa678ae9d95a60be1baf2f377f9
ahyaemon <iamahyaemon@gmail.com>
2019-10-02 22:28:04 +0900

    add electron

--- README.md
+++ README.md
@@ -7,23 +7,11 @@ yarn install
 
 ### Compiles and hot-reloads for development
 ```
-yarn run serve
+yarn electron:serve
 ```
 
 ### Compiles and minifies for production
 ```
-yarn run build
+yarn electron:build
 ```
-
-### Run your tests
-```
-yarn run test
-```
-
-### Lints and fixes files
-```
-yarn run lint
-```
-
-### Customize configuration
-See [Configuration Reference](https://cli.vuejs.org/config/).
+これでdist_electron以下に実行可能ファイルが生成される
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -6,13 +6,20 @@
 </template>
 
 <script lang="ts">
-import { Component, Vue } from 'vue-property-decorator';
-import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+  import { Component, Vue } from 'vue-property-decorator';
+  import HelloWorld from '@/components/HelloWorld.vue';
+  import fs from 'fs'
 
-@Component({
-  components: {
-    HelloWorld,
-  },
-})
-export default class Home extends Vue {}
+  @Component({
+    components: {
+      HelloWorld,
+    },
+  })
+  export default class Home extends Vue {
+    private mounted() {
+      console.log("test")
+      const txt = fs.readFileSync("memo.yml", 'utf8')
+      console.log(txt)
+    }
+  }
 </script>
