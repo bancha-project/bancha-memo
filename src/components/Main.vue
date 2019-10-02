cb493a399ee983f410ba0536c551ead039462d8d
ahyaemon <iamahyaemon@gmail.com>
2019-10-03 07:02:35 +0900

    add opening yml file

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,12 +1,33 @@
 <template>
     <div>
-        <p>Here is main.</p>
+        <button @click="load">load</button>
+        <ul>
+            <li v-for="value, key in yaml">
+                {{ key }}: {{ value }}
+            </li>
+        </ul>
     </div>
 </template>
 
 <script lang="ts">
     import { Component, Vue } from 'vue-property-decorator'
+    import fs from 'fs'
+    import jsYaml from 'js-yaml'
 
     @Component
-    export default class Hello extends Vue {}
+    export default class Hello extends Vue {
+        private yaml = {}
+
+        private load() {
+            const dialog = require('electron').remote.dialog
+            const filepaths = dialog.showOpenDialogSync({
+                properties: ['openFile'],
+                title: 'Select a text file',
+                defaultPath: '.',
+
+            })
+            const txt = fs.readFileSync(filepaths![0], 'utf8')
+            this.yaml = jsYaml.safeLoad(txt)
+        }
+    }
 </script>
