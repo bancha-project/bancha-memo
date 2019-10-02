492c2230e885e0a2934762de76bc8ed189b93428
ahyaemon <iamahyaemon@gmail.com>
2019-10-02 23:21:28 +0900

    add reading yaml

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,26 +1,25 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    <ul>
+      <li v-for="value, key in yaml">
+        {{ key }}, {{ value }}
+      </li>
+    </ul>
   </div>
 </template>
 
 <script lang="ts">
   import { Component, Vue } from 'vue-property-decorator'
-  import HelloWorld from '@/components/HelloWorld.vue'
   import fs from 'fs'
   import jsYaml from 'js-yaml'
 
-  @Component({
-    components: {
-      HelloWorld,
-    },
-  })
+  @Component
   export default class Home extends Vue {
+    private yaml = {}
+
     private mounted() {
       const txt = fs.readFileSync('memo.yml', 'utf8')
-      const yaml = jsYaml.safeLoad(txt)
-      console.log(yaml)
+      this.yaml = jsYaml.safeLoad(txt)
     }
   }
 </script>
