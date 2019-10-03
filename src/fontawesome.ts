7b25fcd08940315db59261929a87a25416c8824a
ahyaemon <iamahyaemon@gmail.com>
2019-10-03 18:00:50 +0900

    delete fontawesome.ts

--- src/fontawesome.ts
+++
@@ -1,7 +0,0 @@
-import { library } from '@fortawesome/fontawesome-svg-core'
-import { faFile, faFolderOpen } from '@fortawesome/free-solid-svg-icons'
-
-library.add(
-    faFolderOpen,
-    faFile,
-)
