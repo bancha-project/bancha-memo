363f9737a208096a0c5eeaea84ce3944c2c3728f
ahyaemon <iamahyaemon@gmail.com>
2019-10-10 22:43:12 +0900

    add filter field

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,6 +1,11 @@
 <template lang="pug">
     main.container.pt-20
         input#copy-area.hidden(type="text")
+        .field
+            .control.has-icons-left
+                input.input.is-info(type="text" placeholder="しぼりこみ")
+                span.icon.is-small.is-left
+                    font-awesome-icon.hover-grey(icon="filter" size="lg")
         div.mb-20(v-for="itemGroup in itemGroups")
             GroupCard(:itemGroup="itemGroup")
 </template>
