f14858b56bc4fb5ccfbfa420e7d5a1e619807b20
ahyaemon <iamahyaemon@gmail.com>
2019-10-14 09:52:40 +0900

    add new file

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -4,6 +4,7 @@
             .container
                 h1.title
                     span bancha memo
+                    font-awesome-icon.hover-grey.ml-10(icon="file" @click="create")
                     font-awesome-icon.hover-grey.ml-10(icon="folder-open" @click="load")
                 h2.subtitle copy memo quickly
 </template>
@@ -11,13 +12,30 @@
 <script lang="ts">
     import { Component, Vue } from 'vue-property-decorator'
     import { appStore } from '@/store'
+    import electron from 'electron'
 
     @Component
     export default class Header extends Vue {
 
+        private create() {
+            const filepath = electron.remote.dialog.showSaveDialogSync({
+                title: 'ファイル作成',
+                message: '新規作成',
+                filters: [
+                    {
+                        name: 'yaml',
+                        extensions: ['yml']
+                    }
+                ]
+            })
+            if (filepath) {
+                appStore.newFile(filepath)
+                appStore.save()
+            }
+        }
+
         private load() {
-            const dialog = require('electron').remote.dialog
-            const filepaths = dialog.showOpenDialogSync({
+            const filepaths = electron.remote.dialog.showOpenDialogSync({
                 properties: ['openFile'],
                 title: 'Select a text file',
                 defaultPath: '.',
--- src/main.ts
+++ src/main.ts
@@ -3,16 +3,25 @@ import App from './App.vue'
 import store from './store'
 import { library } from '@fortawesome/fontawesome-svg-core'
 import {
-  faFolderOpen,
-  faCopy,
-  faFilter,
-  faExternalLinkAlt,
-  faPlusCircle,
-  faTimesCircle,
+    faFolderOpen,
+    faCopy,
+    faFilter,
+    faExternalLinkAlt,
+    faPlusCircle,
+    faTimesCircle,
+    faFile,
 } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 
-library.add(faFolderOpen, faCopy, faFilter, faExternalLinkAlt, faPlusCircle, faTimesCircle)
+library.add(
+    faFolderOpen,
+    faCopy,
+    faFilter,
+    faExternalLinkAlt,
+    faPlusCircle,
+    faTimesCircle,
+    faFile,
+)
 Vue.component('font-awesome-icon', FontAwesomeIcon)
 
 Vue.config.productionTip = false
